2431b247803a6c3720a2b4117053c0c4fe6821cb
Thomas Didriksen <t.didriksen@gmail.com>
2015-03-09 15:38:24 +0100

    Made BuiltinHandlers.hs pass tests.

--- src/Idris/GuardedRecursion/BuiltinHandlers.hs
+++ src/Idris/GuardedRecursion/BuiltinHandlers.hs
@@ -1,9 +1,10 @@
-module Idris.GuardedRecursion.BuiltinHandlers where
-
+{-# LANGUAGE ViewPatterns #-}
 {-|
   This file contains functions for interfacing with the Idris definitions
   from the GuarededRecursion namespace in /libs/prelude/Builtins.idr
 -}
+
+module Idris.GuardedRecursion.BuiltinHandlers where
 
 import Idris.Core.TT
 
@@ -21,6 +22,16 @@ applyUnary t t' = liftM2 App t t'
 applyBinary :: GR Term -> GR Term -> GR Term -> GR Term
 applyBinary a b c = applyUnary (applyUnary a b) c
 
+unapplyUnary :: (Term -> Bool) -> Term -> Maybe Term
+unapplyUnary p (App a b) | p a = Just b
+unapplyUnary _ _ = Nothing
+
+unapplyBinary :: (Term -> Bool) -> Term -> Maybe Term
+unapplyBinary p (App (App a _) b) | p a = Just b
+unapplyBInary _ _ = Nothing
+
+isRef :: Name -> Term -> Bool
+isRef n (P Ref n' _) | n == n' = True
 
 -- 
 
